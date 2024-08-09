8e91e0121b3d2ed6d8636e9f1bded2610bc33852
dsbowman <dsbowman@gmail.com>
2024-08-09 18:16:51 -0400

    Initial Commit

---
+++ CommunityResourceFinder/CommunityResourceFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommunityResourceFinderApp.swift
+//  CommunityResourceFinder
+//
+//  Created by Deke Bowman on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct CommunityResourceFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
