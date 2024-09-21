9b603d7bcb3129f5111fd2820bd081affefcc441
dsbowman <dsbowman@gmail.com>
2024-09-21 19:37:28 -0400

    Removing old commented out code

--- CommunityResourceFinder/Services/NetworkManager.swift
+++ CommunityResourceFinder/Services/NetworkManager.swift
@@ -77,37 +77,5 @@ final class NetworkManager {
     }
     
 }
-
-    
-//    func getData() async throws -> [Record] {
-//        guard let url = URL(string: baseURL) else {
-//            throw RFError.invalidURL
-//        }
-//        
-//        var request = URLRequest(url: url) // Create a mutable request
-//        request.setValue(authorizationToken, forHTTPHeaderField: "Authorization")
-//        request.httpMethod = "GET"
-//        
-//        let (data, response) = try await URLSession.shared.data(for: request)
-//        
-//        if let httpResponse = response as? HTTPURLResponse {
-//            print("Status Code: \(httpResponse.statusCode)")
-////                        if let headers = httpResponse.allHeaderFields as? [String: String] {
-////                            print("Headers: \(headers)")
-////                        }
-//        }
-//        
-//        guard let httpResponse = response as? HTTPURLResponse,
-//              (200...299).contains(httpResponse.statusCode) else {
-//            throw RFError.invalidResponse // Or a more specific error based on the response
-//        }
-//        
-//        do {
-//            let decoder = JSONDecoder()
-//            return try decoder.decode(CommunityResourceModel.self, from: data).records
-//        } catch {
-//            throw RFError.invalidURL // Provide the underlying decoding error
-//        }
-//    }
     
 
