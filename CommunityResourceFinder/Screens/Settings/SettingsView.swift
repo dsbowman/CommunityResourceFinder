8ff7a78d2ced20d954b44658992c1f585e0d0421
dsbowman <dsbowman@gmail.com>
2025-04-28 17:49:24 -0400

    Updating location "!" optional error on location.

---
+++ CommunityResourceFinder/Screens/Settings/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  CommunityResourceFinder
+//
+//  Created by Deke Bowman on 4/28/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
